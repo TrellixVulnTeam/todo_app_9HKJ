946e6c8fdc598467b05958429a0804847da72890
wikedzi <twikedzi@gmail.com>
2017-02-17 10:05:28 +0300

    Modified the model class

--- model.py
+++ model.py
@@ -5,8 +5,8 @@ from pymongo import MongoClient
 class Model():
 
     def __init__(self):
-        self.client = MongoClient("mongodb://52.26.59.112:27017")
-        #self.client = MongoClient()
+        #self.client = MongoClient("mongodb://52.26.59.112:27017")
+        self.client = MongoClient()
         self.db = self.client['todoDB']
         self.todos = self.db['todos']
 
