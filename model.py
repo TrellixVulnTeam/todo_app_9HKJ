66b61b5d262a1e42f0306068ac9b66079d87b0d2
wikedzi <twikedzi@gmail.com>
2017-02-17 10:23:48 +0300

    Modified the Mongo Server Path in Model Class

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ class Model():
 
     def __init__(self):
         #self.client = MongoClient("mongodb://52.26.59.112:27017")
-        self.client = MongoClient()
+        self.client = MongoClient("mongodb://127.0.0.1:27017")
         self.db = self.client['todoDB']
         self.todos = self.db['todos']
 
